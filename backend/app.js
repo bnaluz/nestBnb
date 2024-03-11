426b5b2f734e1b992d772a30fdfae56a8a61862c
Bobby Naluz <bnaluz9@gmail.com>
2024-03-11 18:34:27 -0400

    add: error middlewares

--- backend/app.js
+++ backend/app.js
@@ -46,4 +46,42 @@ app.use(
 const routes = require('./routes');
 app.use(routes);
 
+//*error handling middlewares
+
+//* regular middleware that catches any requests that dont match any of the defined routes and creates a server error with a status code of 404 - the next will pass the error status on to the error handling middleware
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = 'Resource Not Found';
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+const { ValidationError } = require('sequelize');
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
 module.exports = app;
