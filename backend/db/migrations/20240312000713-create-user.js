b7556fa14f8a8088a00ca743a1f548f194dc0c5b
Bobby Naluz <bnaluz9@gmail.com>
2024-03-14 19:49:59 -0400

    fix:create-user table migration

--- backend/db/migrations/20240312000713-create-user.js
+++ backend/db/migrations/20240312000713-create-user.js
@@ -53,6 +53,7 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = 'Users';
+    return queryInterface.dropTable(options);
   },
 };
