d080c465e6db87f2eff1aec6a39223f6ee235624
Bobby Naluz <bnaluz9@gmail.com>
2024-03-14 18:50:14 -0400

    add firstname/lastname

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -41,6 +41,8 @@ router.post('/', validateLogin, async (req, res, next) => {
   const safeUser = {
     id: user.id,
     email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
     username: user.username,
   };
 
