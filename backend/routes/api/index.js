19639d43e94947c5a4862b87312b2de282c78a8e
Bobby Naluz <bnaluz9@gmail.com>
2024-04-03 22:04:01 -0400

    starting booking routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js');
 
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
@@ -36,6 +37,8 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 
 router.use('/reviews', reviewsRouter);
+
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
