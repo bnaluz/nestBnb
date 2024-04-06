e55b86b18e25bab984885d251e4cc33f75c2609c
Bobby Naluz <bnaluz9@gmail.com>
2024-04-06 16:41:54 -0400

    fix migration for bookings

--- backend/db/migrations/20240402230629-create-booking.js
+++ backend/db/migrations/20240402230629-create-booking.js
@@ -1,5 +1,9 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bookings', {
@@ -38,6 +42,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = 'Bookings';
+    return queryInterface.dropTable(options);
   },
 };
