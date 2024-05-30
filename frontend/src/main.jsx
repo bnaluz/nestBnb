faac203adc7c74658cf180fff523d0a0f25f1fd6
Bobby Naluz <bnaluz9@gmail.com>
2024-05-30 18:39:00 -0400

    add: csrf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,18 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
