2efcb962590a9eb18a91c94379b012c8107f054a
Bobby Naluz <bnaluz9@gmail.com>
2024-06-14 11:02:27 -0400

    add preview image back

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -251,7 +251,7 @@ router.get('/:spotId', async (req, res) => {
       'price',
       'createdAt',
       'updatedAt',
-      // 'preview_image',
+      'preview_image',
       [Sequelize.fn('COUNT', Sequelize.col('Reviews.id')), 'numReviews'],
       [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgStarRating'],
     ],
@@ -286,7 +286,7 @@ router.get('/:spotId', async (req, res) => {
       updatedAt: createdAndUpdatedFormatter(spotData.updatedAt),
       numReviews: numReviews,
       avgRating: avgRating,
-      // previewImage: spotData.preview_image || 'defaultURL',
+      previewImage: spotData.preview_image || 'defaultURL',
       SpotImages: spotData.SpotImages.map((img) => ({
         id: img.id,
         url: img.url,
